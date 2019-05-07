f137d7bd9e470f2d0022de188acd4480f0510145
UMUNNAKWE, JOHNLEONARD CHINAEMEREM <johnleonarduc@gmail.com>
2019-05-07 11:19:05 +0100

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let menuBtn = document.getElementById('navicon');
 let menuList = document.getElementById('primaryNav');
+
 menuBtn.addEventListener('click', function(e){
     menuList.classList.toggle("closed");
     e.stopPropagation;
---
+++ js/slicksliderinit.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.myslider').slick({
+        dots: true,
+        autoplay: true,
+        adaptiveHeight: true,
+        arrows: true,
+        swipeToSlide: true,
+        autoplaySpeed: 1000
+    });
+});
