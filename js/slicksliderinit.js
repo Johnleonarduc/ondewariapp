be0c62e5be1c30ae646df8b0818ca6ca94f5eef3
UMUNNAKWE, JOHNLEONARD CHINAEMEREM <johnleonarduc@gmail.com>
2019-05-11 22:44:10 +0100

    Add files via upload

--- js/slicksliderinit.js
+++ js/slicksliderinit.js
@@ -6,5 +6,21 @@ $(document).ready(function(){
         arrows: true,
         swipeToSlide: true,
         autoplaySpeed: 1000
+
     });
+   /* $('.portfolio-slider1').slick({
+        autoplay: true,
+        adaptiveHeight: true,
+        swipeToSlide: true,
+        autoplaySpeed: 1000,
+        speed: 1500,
+        fade: true,
+    });
+    $('.portfolio-slider2').slick({
+        autoplay: true,
+        adaptiveHeight: true,
+        swipeToSlide: true,
+        autoplaySpeed: 1000
+
+    }); */
 });
