911f5f407b96fd82fa88126be683afcc1eb66b37
Johnleonarduc <johnleonarduc@gmail.com>
2019-05-14 23:11:57 +0100

    fix opera hambugger toggle problem

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,16 @@ let menuBtn = document.getElementById('navicon');
 let menuIcon = menuBtn.firstChild.nextSibling;
 let menuList = document.getElementById('primaryNav');
 let clickCount = 0;
-console.log(menuIcon.classList.contains("fa-bars"));
 
 menuBtn.addEventListener('click', function(e){
     clickCount +=1;
-    menuList.classList.toggle("closed");
     if(clickCount === 1){
-        menuIcon.classList.remove("fa-bars");
-        menuIcon.classList.add("fa-times");
+        menuIcon.className = "fas fa-times";
+        menuList.className = "open";
     }
     else{
-        menuIcon.classList.remove("fa-times");
-        menuIcon.classList.add("fa-bars");
+        menuIcon.className = "fas fa-bars";
+        menuList.className = "closed";
         clickCount = 0;
     }
 
